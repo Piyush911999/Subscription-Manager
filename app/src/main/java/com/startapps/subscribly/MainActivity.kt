91bd4ea4e618080194c0c322ed4a54178197fb7d
Piyush911999 <piyushkumar911999@gmail.com>
2020-05-21 16:09:04 +0530

    initial commit

---
+++ app/src/main/java/com/startapps/subscribly/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.startapps.subscribly
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.startapps.subscribly.base.BaseActivity
+
+class MainActivity : BaseActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
